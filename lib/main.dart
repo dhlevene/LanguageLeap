c6384de7de0ab0b116332e3f6b1905ce71f46e7a
Bailey Waldorf <baileywaldorf@github.com>
2019-01-19 16:58:08 -0500

    ability to save images to external storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:path_provider/path_provider.dart';
 import 'dart:async';
+import 'dart:io';
 
 List<CameraDescription> cameras;
 
 Future<void> main() async {
   cameras = await availableCameras();
   runApp(MyApp());
+}
+
+/// Returns a suitable camera icon for [direction].
+IconData getCameraLensIcon(CameraLensDirection direction) {
+  switch (direction) {
+    case CameraLensDirection.back:
+      return Icons.camera_rear;
+    case CameraLensDirection.front:
+      return Icons.camera_front;
+    case CameraLensDirection.external:
+      return Icons.camera;
+  }
+  throw ArgumentError('Unknown lens direction');
 }
 
 class MyApp extends StatelessWidget {
@@ -35,10 +50,15 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+void logError(String code, String message) =>
+    print('Error: $code\nError Message: $message');
+
 class _MyHomePageState extends State<MyHomePage> {
   int _score = 0;
   int _numWrong = 0;
   int _numRight = 0;
+  String imagePath;
+  CameraController controller;
 
   void _onCorrect() {
     setState(() {
@@ -53,7 +73,6 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  CameraController controller;
   @override
   void initState() {
     super.initState();
@@ -70,6 +89,135 @@ class _MyHomePageState extends State<MyHomePage> {
   void dispose() {
     controller?.dispose();
     super.dispose();
+  }
+
+  void onTakePictureButtonPressed() {
+    print('took a picture!');
+    takePicture().then((String filePath) {
+      if (mounted) {
+        setState(() {
+          imagePath = filePath;
+        });
+      }
+    });
+  }  
+
+  Widget _cameraPreviewWidget() {
+    if (controller == null || !controller.value.isInitialized) {
+      return const Text(
+        'Tap a camera',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24.0,
+          fontWeight: FontWeight.w900,
+        ),
+      );
+    } else {
+      return AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: CameraPreview(controller),
+      );
+    }
+  }
+
+  Widget _captureIcon() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.camera_alt),
+          color: Colors.blue,
+          onPressed: controller != null &&
+                  controller.value.isInitialized &&
+                  !controller.value.isRecordingVideo
+              ? onTakePictureButtonPressed
+              : null,
+        ),
+      ],
+    );
+  }
+
+  String timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
+
+  void _showCameraException(CameraException e) {
+    logError(e.code, e.description);
+  }
+
+  void onNewCameraSelected(CameraDescription cameraDescription) async {
+    if (controller != null) {
+      await controller.dispose();
+    }
+    controller = CameraController(cameraDescription, ResolutionPreset.high);
+
+    // If the controller is updated then update the UI.
+    controller.addListener(() {
+      if (mounted) setState(() {});
+    });
+
+    try {
+      await controller.initialize();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future<String> takePicture() async {
+    if (!controller.value.isInitialized) {
+      return null;
+    }
+    print('inside take picture function!');
+    final Directory extDir = await getExternalStorageDirectory();
+    final String dirPath = '${extDir.path}/Pictures/flutter_test';
+    await Directory(dirPath).create(recursive: true);
+    final String filePath = '$dirPath/${timestamp()}.jpg';
+    print('path: $filePath');
+
+    if (controller.value.isTakingPicture) {
+      // A capture is already pending, do nothing.
+      return null;
+    }
+
+    try {
+      await controller.takePicture(filePath);
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return null;
+    }
+    return filePath;
+  }
+
+  Widget camera() {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: Center(
+                  child: _cameraPreviewWidget(),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                border: Border.all(
+                  color: controller != null && controller.value.isRecordingVideo
+                      ? Colors.redAccent
+                      : Colors.grey,
+                  width: 3.0,
+                ),
+              ),
+            ),
+          ),
+          _captureIcon(),
+        ],
+      ),
+    );
   }
 
   @override
@@ -104,7 +252,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     width: 300.0,
                     child: AspectRatio(
                       aspectRatio: controller.value.aspectRatio,
-                      child: CameraPreview(controller)
+                      child: camera(),
                     ),
                   ),
                 ),
